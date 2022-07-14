66266cb2cca383493bb6c313c44fda1cea4d5e55
chidnk <chidnk@devcamp.edu.vn>
2022-07-14 15:13:28 +0700

    commit

--- src/components/ContentComponent.js
+++ src/components/ContentComponent.js
@@ -1,31 +1,38 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { Row, Col } from "react-bootstrap";
-import {Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 import Pagination from '@mui/material/Pagination';
 
 function ContentComponent() {
-    const { productList } = useSelector((reduxData) => reduxData.reducers);
+    const { productList, lowPrice, highPrice } = useSelector((reduxData) => reduxData.reducers);
 
     const limit = 12;
-    
+
     const [pageIndex, setPageIndex] = useState(1);
 
     const [pageAmount, setPageAmount] = useState(0);
 
-    const [rows, setRows] = useState(null)
+    const [data, setData] = useState([])
+
+    const [rows, setRows] = useState(null);
 
     const onPageIndexChange = (event, value) => {
         setPageIndex(value);
         window.scrollTo({
             top: 0,
             behavior: 'smooth'
-          });
+        });
     }
 
     useEffect(() => {
-        setPageAmount(Math.ceil(productList.length / limit));
-        setRows(productList.slice((pageIndex - 1) * limit, pageIndex * limit))
+        setData(productList.filter((element, index) => {
+            return element.promotionPrice >= lowPrice && element.promotionPrice <= highPrice
+        }))
+
+        setPageAmount(Math.ceil(data.length / limit));
+        setRows(data.slice((pageIndex - 1) * limit, pageIndex * limit));
+
     })
     return (
         <>
@@ -34,22 +41,25 @@ function ContentComponent() {
                     rows !== null
                         ?
                         rows.map((element, index) => {
-                            return (
-                                <Col className="col-3 mt-3 mb-4">
-                                    <Row className="text-center p-0 m-0">
-                                        <img
-                                            src={element.imageUrl}
-                                            className="p-0"
-                                            style={{ width: "300px", height: "300px", objectFit: "cover" }}
-                                        />
-                                        <p className="h5 mt-1">{element.name}</p>
-                                        <p>
-                                            <a className="old-price">{element.buyPrice}</a>&nbsp;
-                                            <a className="new-price">${element.promotionPrice}</a>
-                                        </p>
-                                    </Row>
-                                </Col>
-                            )
+                            if (element.promotionPrice >= lowPrice && element.promotionPrice <= highPrice) {
+                                return (
+                                    <Col className="col-4 mt-3 mb-4">
+                                        <Row className="text-center p-0 m-0">
+                                            <img
+                                                src={element.imageUrl}
+                                                className="p-1"
+                                                style={{ width: "100%", height: "300px", objectFit: "cover" }}
+                                            />
+                                            <p className="h5 mt-1">{element.name}</p>
+                                            <p>
+                                                <a className="old-price">{element.buyPrice}</a>&nbsp;
+                                                <a className="new-price">${element.promotionPrice}</a>
+                                            </p>
+                                        </Row>
+                                    </Col>
+                                )
+                            }
+
                         })
                         :
                         null
@@ -57,7 +67,7 @@ function ContentComponent() {
             </Row>
 
             {/* Pagination */}
-            <Grid className="mb-5 d-flex justify-content-center">
+            <Grid className="mb-5 d-flex justify-content-end">
                 <Pagination count={pageAmount} defaultPage={pageIndex} onChange={onPageIndexChange} />
             </Grid>
         </>
