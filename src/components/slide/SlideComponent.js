4cb4df58fafecc5f005b998454bbe5ccc75fde99
chidnk <chidnk@devcamp.edu.vn>
2022-08-08 20:04:31 +0700

    commit

--- src/components/slide/SlideComponent.js
+++ src/components/slide/SlideComponent.js
@@ -14,27 +14,14 @@ function SlideComponent() {
         <>
             <Container className="mt-2">
                 <Row className="align-items-center">
-                    <Col className="col-6 p-5">
-                        <Row>
-                            <h1>Jadu Shop</h1>
-                        </Row>
-                        <Typography className="text-secondary">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                        </Typography>
-                        <Row className="mt-3">
-                            <Col className="col-4">
-                                <button className="btn border-primary text-primary btn-lg" onClick={onBtnShopNowClick}>SHOP NOW</button>
-                            </Col>
-                        </Row>
-                    </Col>
-                    <Col className="col-6">
+                    <Grid className="col-carousel">
                         <Carousel>
                             <Carousel.Item>
                                 <img
                                     className="w-100"
                                     src={require("../../app/image/slide/slide1.jpg")}
                                     alt="First slide"
-                                    style={{width : "500px", height : "500px", objectFit: "contain"}}
+                                    style={{ width: "100%", objectFit: "contain" }}
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
@@ -42,7 +29,7 @@ function SlideComponent() {
                                     className="d-block w-100"
                                     src={require("../../app/image/slide/slide2.jpg")}
                                     alt="Second slide"
-                                    style={{width : "500px", height : "500px", objectFit: "contain"}}
+                                    style={{ width: "100%", objectFit: "contain" }}
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
@@ -50,7 +37,7 @@ function SlideComponent() {
                                     className="d-block w-100"
                                     src={require("../../app/image/slide/slide3.jpg")}
                                     alt="Third slide"
-                                    style={{width : "500px", height : "500px", objectFit: "contain"}}
+                                    style={{ width: "100%", objectFit: "contain" }}
                                 />
                             </Carousel.Item>
                             <Carousel.Item>
@@ -58,11 +45,24 @@ function SlideComponent() {
                                     className="d-block w-100"
                                     src={require("../../app/image/slide/slide4.jpg")}
                                     alt="Fourth slide"
-                                    style={{width : "500px", height : "500px", objectFit: "contain"}}
+                                    style={{ width: "100%", objectFit: "contain" }}
                                 />
                             </Carousel.Item>
                         </Carousel>
-                    </Col>
+                    </Grid>
+                    <Grid className="col-carousel">
+                        <Row>
+                            <h1>Jadu Shop</h1>
+                        </Row>
+                        <Typography className="text-secondary">
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                        </Typography>
+                        <Row className="mt-3">
+                            <Col className="col-4">
+                                <button className="btn border-primary text-primary btn-lg" onClick={onBtnShopNowClick}>SHOP NOW</button>
+                            </Col>
+                        </Row>
+                    </Grid>
                 </Row>
             </Container>
         </>
