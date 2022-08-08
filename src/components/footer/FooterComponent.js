fd7ffab0c5272b4b369939fe9e808f96c790b73f
chidnk <chidnk@devcamp.edu.vn>
2022-08-08 22:12:57 +0700

    commit

--- src/components/footer/FooterComponent.js
+++ src/components/footer/FooterComponent.js
@@ -5,38 +5,38 @@ import logoImg from "../../app/image/logo-white.PNG";
 function FooterComponent() {
     return (
         <>
-            <Grid style={{ height: "180px" }}>
+            <Grid className="mb-2">
                 <Grid className="container">
                     <Row className="pt-2">
                         <Col className="col-3">
-                            <h5>PRODUCTS</h5>
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Help Center</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Contact Us</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Product Help</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Warranty</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Order Status</a><br />
+                            <a className="footer-title">PRODUCTS</a><br />
+                            <a href="#" className="footer-content">Help Center</a><br />
+                            <a href="#" className="footer-content">Contact Us</a><br />
+                            <a href="#" className="footer-content">Product Help</a><br />
+                            <a href="#" className="footer-content">Warranty</a><br />
+                            <a href="#" className="footer-content">Order Status</a><br />
                         </Col>
                         <Col className="col-3">
-                            <h5>SERVICES</h5>
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Help Center</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Contact Us</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Product Help</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Warranty</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Order Status</a><br />
+                            <a className="footer-title">SERVICES</a><br />
+                            <a href="#" className="footer-content">Help Center</a><br />
+                            <a href="#" className="footer-content">Contact Us</a><br />
+                            <a href="#" className="footer-content">Product Help</a><br />
+                            <a href="#" className="footer-content">Warranty</a><br />
+                            <a href="#" className="footer-content">Order Status</a><br />
                         </Col>
                         <Col className="col-3">
-                            <h5>SUPPORT</h5>
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Help Center</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Contact Us</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Product Help</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Warranty</a><br />
-                            <a className="text-secondary" href="#" style={{ textDecoration: "none" }}>Order Status</a><br />
+                            <a className="footer-title">SUPPORT</a><br />
+                            <a href="#" className="footer-content">Help Center</a><br />
+                            <a href="#" className="footer-content">Contact Us</a><br />
+                            <a href="#" className="footer-content">Product Help</a><br />
+                            <a href="#" className="footer-content">Warranty</a><br />
+                            <a href="#" className="footer-content">Order Status</a><br />
                         </Col>
-                        <Col className="col-3 justify-content-center align-items-center">
-                            <Row className="justify-content-center">
-                                <img src={logoImg} className="logo-style w-75" />
+                        <Col className="col-3 d-flex align-items-end flex-column">
+                            <Row className="footer-logo">
+                                <Grid className="square" style={{backgroundImage: "url(" + logoImg + ")"}}></Grid>
                             </Row>
-                            <Row>
+                            <Row className="footer-logo">
                                 <Col className="d-flex justify-content-center">
                                     <i className="fa-brands fa-facebook m-2 fa-lg"></i>&nbsp;
                                     <i className="fa-brands fa-instagram m-2 fa-lg"></i>&nbsp;
@@ -44,7 +44,6 @@ function FooterComponent() {
                                     <i className="fa-brands fa-twitter m-2 fa-lg"></i>&nbsp;
                                 </Col>
                             </Row>
-
                         </Col>
                     </Row>
                 </Grid>
